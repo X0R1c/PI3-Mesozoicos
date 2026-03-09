142b84b4ef78efd5ba06fb5bdeb94c3b65246a3c
x86PHS <rique.souza06@icloud.com>
2026-03-08 21:36:28 -0300

    Status: Atualizado. Com a DLL do jogo juntamente dos arquivos necessários dipostos pelo Coordenador do PI.

--- Form1.cs
+++ Form1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Draft;
 
 namespace Mesozoicos {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+        
         }
     }
 }
